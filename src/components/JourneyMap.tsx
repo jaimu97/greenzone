56556c8e2715a4a2f22d76c30889a994d48c07fb
Jai Mu <kwibjo@hotmail.com.au>
2024-08-25 18:38:22 +0930

    Update Android build.

--- src/components/JourneyMap.tsx
+++ src/components/JourneyMap.tsx
@@ -1,36 +1,45 @@
-import React, { useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
-import { useIonViewWillEnter } from '@ionic/react';
+import React, { useEffect, useRef, useState } from 'react';
+import { MapContainer, TileLayer, Polyline, useMap } from 'react-leaflet';
+import { useIonViewDidEnter } from '@ionic/react';
 import 'leaflet/dist/leaflet.css';
 
 interface JourneyMapProps {
   positions: [number, number][];
 }
 
+// Stupid hack to fix the maps rendering incorrectly.
+const MapUpdater: React.FC = () => {
+  const map = useMap();
+  useEffect(() => {
+    map.invalidateSize();
+  }, [map]);
+  return null;
+};
+
 const JourneyMap: React.FC<JourneyMapProps> = ({ positions }) => {
   const mapRef = useRef<any>(null);
+  const [key, setKey] = useState(0);
   const centre: [number, number] = [-12.35556, 130.88253]; // TODO: Calculate the centre based on the positions
   const redOptions = { color: 'red' };
 
-  // Stupid hack to fix the maps rendering incorrectly.
-  useIonViewWillEnter(() => {
-    if (mapRef.current) {
-      window.dispatchEvent(new Event('resize'));
-      mapRef.current.invalidateSize();
-    }
+  useIonViewDidEnter(() => {
+    setKey(prev => prev + 1);
   });
 
   useEffect(() => {
-    if (mapRef.current) {
-      window.dispatchEvent(new Event('resize'));
-      mapRef.current.invalidateSize();
-    }
-  }, []);
+    const timer = setTimeout(() => {
+      if (mapRef.current) {
+        mapRef.current.invalidateSize();
+      }
+    }, 100);
+    return () => clearTimeout(timer);
+  }, [key]);
 
   return (
     <MapContainer
+      key={key}
       center={centre}
-      zoom={15}
+      zoom={16}
       scrollWheelZoom={false}
       style={{ height: '200px', width: '100%' }}
       ref={mapRef}
@@ -40,6 +49,7 @@ const JourneyMap: React.FC<JourneyMapProps> = ({ positions }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Polyline pathOptions={redOptions} positions={positions} />
+      <MapUpdater />
     </MapContainer>
   );
 };
