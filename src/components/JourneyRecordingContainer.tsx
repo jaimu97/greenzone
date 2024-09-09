99be52616613781b7a2915c195dd7f3c79c5b96d
Jai Mu <kwibjo@hotmail.com.au>
2024-09-09 14:27:00 +0930

    Remove journeyId from modals arguments. Not needed since we don't know the id anyway until *after* the main journey is uploaded.

--- src/components/JourneyRecordingContainer.tsx
+++ src/components/JourneyRecordingContainer.tsx
@@ -19,12 +19,11 @@ import FeedbackModal from "./FeedbackModal";
 interface JourneyRecordingProps {
   onEndJourney: () => void; // function is called when the journey ends
   user: any;
-  journeyId: number; // for passing to feedback modal
 }
 
 // Documentation: https://ionicframework.com/docs/native/geolocation
 
-const JourneyRecordingContainer: React.FC<JourneyRecordingProps> = ({ onEndJourney, user, journeyId }) => {
+const JourneyRecordingContainer: React.FC<JourneyRecordingProps> = ({ onEndJourney, user }) => {
   const [currentPosition, setCurrentPosition] = useState<Position | null>(null);
   const [positions, setPositions] = useState<[number, number][]>([]);
   const [permissionStatus, setPermissionStatus] = useState<any>(null);
@@ -211,7 +210,6 @@ const JourneyRecordingContainer: React.FC<JourneyRecordingProps> = ({ onEndJourn
         isOpen={isFeedbackModalOpen}
         onClose={() => setIsFeedbackModalOpen(false)}
         userId={user.id}
-        journeyId={journeyId}
       />
     </>
   );
