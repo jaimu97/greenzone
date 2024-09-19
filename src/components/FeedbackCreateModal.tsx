b0bbf37a4da6fa8f34787cc4d2780a303ff60515
Jai Mu <kwibjo@hotmail.com.au>
2024-09-19 15:54:22 +0930

    Forgot to clear feedback from localstorage when submitted and added toast to say feedback recorded. Disgusted in myself for making this. Will need to do better once testing is, and we can implement feedback from our users.

--- src/components/FeedbackCreateModal.tsx
+++ src/components/FeedbackCreateModal.tsx
@@ -10,6 +10,7 @@ import {
   IonItem,
   IonLabel,
   IonImg,
+  IonToast
 } from '@ionic/react';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
@@ -24,6 +25,7 @@ const FeedbackCreateModal: React.FC<FeedbackModalProps> = ({ isOpen, onClose, us
   const [feedback, setFeedback] = useState('');
   const [capturedImage, setCapturedImage] = useState<string | null>(null);
   const textareaRef = useRef<HTMLIonTextareaElement>(null);
+  const [showToast, setShowToast] = useState(false); // Feedback from Mark, needs toast to say feedback was recorded.
 
   useEffect(() => {
     const savedFeedback = localStorage.getItem('tempFeedback');
@@ -86,47 +88,67 @@ const FeedbackCreateModal: React.FC<FeedbackModalProps> = ({ isOpen, onClose, us
       localStorage.setItem('journeyFeedback', JSON.stringify(existingFeedback));
 
       console.log('Feedback stored locally:', feedbackData);
-      setFeedback('');
-      setCapturedImage(null);
-      onClose();
+      setShowToast(true);
+      /* FIXME: Delays modal closing by 1.5 seconds to display this toast. Should be showing the toast in the parent
+       *   `JourneyRecordingContainer.tsx`. Will do once testing is complete, needed this implemented ASAP.
+       */
+      setTimeout(() => {
+        clearFeedbackAndClose();
+      }, 1500);
     }
   };
 
+  const clearFeedbackAndClose = () => {
+    setFeedback('');
+    setCapturedImage(null);
+    localStorage.removeItem('tempFeedback');
+    onClose();
+  };
+
   return (
-    <IonModal isOpen={isOpen} onDidDismiss={onClose}>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Add Feedback</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      {/* FIXME: Modal will delete any text if the textbox scrolls off the screen.
-        *   useEffect is the solution I can think of, however, I had some problems with it 'glitching' and overwriting
-        *   text that had been entered due to it not updating every time a letter is added.
-        */}
-      <IonContent className="ion-padding">
-        <IonItem>
-          <IonLabel position="stacked">Your Feedback</IonLabel>
-          <IonTextarea
-            ref={textareaRef}
-            onIonChange={handleFeedbackChange}
-            placeholder="Enter your feedback here..."
-            rows={6}
-          />
-        </IonItem>
-        {capturedImage && (
-          <IonImg src={capturedImage} className="ion-margin-top" />
-        )}
-        <IonButton expand="block" onClick={takePhoto} className="ion-margin-top">
-          Take Photo
-        </IonButton>
-        <IonButton expand="block" onClick={handleSubmit} className="ion-margin-top">
-          Submit Feedback
-        </IonButton>
-        <IonButton expand="block" onClick={onClose} color="light" className="ion-margin-top">
-          Cancel
-        </IonButton>
-      </IonContent>
-    </IonModal>
+    <>
+      <IonModal isOpen={isOpen} onDidDismiss={clearFeedbackAndClose}>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Add Feedback</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        {/* FIXME: Modal will delete any text if the textbox scrolls off the screen.
+          *   useEffect is the solution I can think of, however, I had some problems with it 'glitching' and overwriting
+          *   text that had been entered due to it not updating every time a letter is added.
+          */}
+        <IonContent className="ion-padding">
+          <IonItem>
+            <IonLabel position="stacked">Your Feedback</IonLabel>
+            <IonTextarea
+              ref={textareaRef}
+              onIonChange={handleFeedbackChange}
+              placeholder="Enter your feedback here..."
+              rows={6}
+            />
+          </IonItem>
+          {capturedImage && (
+            <IonImg src={capturedImage} className="ion-margin-top" />
+          )}
+          <IonButton expand="block" onClick={takePhoto} className="ion-margin-top">
+            Take Photo
+          </IonButton>
+          <IonButton expand="block" onClick={handleSubmit} className="ion-margin-top">
+            Submit Feedback
+          </IonButton>
+          <IonButton expand="block" onClick={clearFeedbackAndClose} color="light" className="ion-margin-top">
+            Cancel
+          </IonButton>
+        </IonContent>
+      </IonModal>
+      <IonToast
+        isOpen={showToast}
+        onDidDismiss={() => setShowToast(false)}
+        message="Feedback recorded!"
+        duration={1500}
+        position="bottom"
+      />
+    </>
   );
 };
 
